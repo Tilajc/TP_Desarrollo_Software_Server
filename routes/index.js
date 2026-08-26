ae39f6f82f05ac02a18e5155c9010fdcff2a2912
tilajc <jctilatti@gmail.com>
2026-08-25 21:38:42 -0300

    topic CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import subjectRoutes from "./subject.js";
+import topicRoutes from "./topic.js";
 // import userRoutes from "./user.js";
 // import classRoutes from "./class.js";
 
 const router = Router();
 
 router.use("/subjects", subjectRoutes);
+router.use("/topics", topicRoutes);
 // router.use("/users", userRoutes);
 // router.use("/classes", classRoutes);
 
